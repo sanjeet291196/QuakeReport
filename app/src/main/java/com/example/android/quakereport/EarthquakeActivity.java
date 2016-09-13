7b4228a59956f7b72df55cfb3544da18ee7d6d7e
sanjit <sanjeet291196@gmail.com>
2016-09-13 19:08:08 +0530

    Menu added

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -19,12 +19,17 @@ import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -37,6 +42,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     ArrayList<EarthquakeItem> earthquakes = null;
     ListView earthquakeListView;
     TextView emptyView;
+    ProgressBar progressBar;
 
     // Create a new {@link ArrayAdapter} of earthquakes
     EarthquakeAdapter adapter;
@@ -51,6 +57,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         // Find a reference to the {@link ListView} in the layout
         earthquakeListView = (ListView) findViewById(R.id.list);
         emptyView = (TextView) findViewById(R.id.empty_view);
+        progressBar = (ProgressBar) findViewById(R.id.loader);
 
         earthquakes = new ArrayList<>();
 
@@ -76,13 +83,35 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
 
         LoaderManager loaderManager = getLoaderManager();
 
-        loaderManager.initLoader(1, null, this);
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            loaderManager.initLoader(1, null, this);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            emptyView.setText(R.string.no_internet);
+        }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public Loader<List<EarthquakeItem>> onCreateLoader(int i, Bundle bundle) {
-        emptyView.setText(R.string.loading);
         return new EarthquakeLoader(this, url);
     }
 
@@ -93,6 +122,13 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
             adapter.addAll(earthquakeItems);
         }
         emptyView.setText(R.string.no_earthquake_data);
+        progressBar.setVisibility(View.GONE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo == null || !networkInfo.isConnected()) {
+            progressBar.setVisibility(View.GONE);
+            emptyView.setText(R.string.no_internet);
+        }
     }
 
     @Override
